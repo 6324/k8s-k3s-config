49ff43d187894f9a5225157acc1184494f8f180c
he.zhao <Zhaohe111>
2022-11-30 22:56:43 +0800

    Change

--- k8s-server/src/main/java/com/example/k8sserver/DemoApi.java
+++ k8s-server/src/main/java/com/example/k8sserver/DemoApi.java
@@ -1,8 +1,7 @@
 package com.example.k8sserver;
 
+import cn.hutool.core.util.RandomUtil;
 import cn.hutool.system.HostInfo;
-import cn.hutool.system.JvmInfo;
-import cn.hutool.system.JvmSpecInfo;
 import cn.hutool.system.SystemUtil;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,19 +20,50 @@ public class DemoApi {
         return "ok|hostname: [" + name + "]  address: [" + address + "] ";
     }
 
-    @GetMapping("/create-memory")
-    public String createMemory(@RequestParam(defaultValue = "1000") int byteSize) throws InterruptedException {
-        byte[] b = new byte[byteSize];
-        System.out.println(b.length);
-        TimeUnit.SECONDS.sleep(30);
-        JvmInfo jvmInfo = SystemUtil.getJvmInfo();
-        JvmSpecInfo jvmSpecInfo = SystemUtil.getJvmSpecInfo();
-        long freeMemory = SystemUtil.getFreeMemory() / 1024 / 1024;
-        long totalMemory = SystemUtil.getTotalMemory() / 1024 / 1024;
-        long maxMemory = SystemUtil.getMaxMemory() / 1024 / 1024;
-        System.out.println(totalMemory);
-        System.out.println(maxMemory);
-        System.out.println(freeMemory);
-        return "ok ";
+    @GetMapping("/up-memory")
+    public String upMemory(@RequestParam(defaultValue = "1") int tag) throws InterruptedException {
+        System.out.println("start >" + SystemUtil.getFreeMemory() / 1024.0 / 1024.0);
+        byte[] b = new byte[tag * 1024 * 1024];
+        System.out.println("end   >" + SystemUtil.getFreeMemory() / 1024.0 / 1024.0);
+        TimeUnit.SECONDS.sleep(5);
+        return "ok";
+    }
+
+
+    static volatile boolean startWhile = true;
+
+    @GetMapping("/up-cpu")
+    public String upCpu(@RequestParam(defaultValue = "1") int tag) {
+        startWhile = true;
+        for (int i = 0; i < tag; i++) {
+            new Thread(() -> {
+                while (startWhile) {
+                    double v = rand_pi(RandomUtil.randomInt(500, 1000));
+                    System.out.println(Thread.currentThread().getName() + " " + v);
+                }
+            }, RandomUtil.randomString(5)).start();
+        }
+        return "ok";
+    }
+
+    @GetMapping("/down-cpu")
+    public String downCpu() {
+        startWhile = false;
+        return "ok";
+    }
+
+    public static double rand_pi(int n) {
+        int numInCircle = 0;
+        double x, y;
+        double pi;
+        for (int i = 0; i < n; i++) {
+            x = Math.random();
+            y = Math.random();
+            if (x * x + y * y < 1) {
+                numInCircle++;
+            }
+        }
+        pi = (4.0 * numInCircle) / n;
+        return pi;
     }
 }
